067d580cf4960f33b75e6f85f19c9197ec8b9067
jake <palatable7@naver.com>
2023-03-31 20:44:46 +0900

    implement

---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+SERVICE_DIR=~/Library/Services/
+
+# shortcut: https://ios-development.tistory.com/1321
+short_cut = "^\$@1" # ctrl + shift + cmd + 1
+
+# copying automator workflow
+echo "copying Service into ~/Library/Services/ ..."
+cp -aR ./my\ custom\ shortcut.workflow $SERVICE_DIR
+
+# copying source to the services folder
+echo "copying myCustomShortcut.sh into ~/Library/Services/ ..."
+cp ./myCustomShortcut.sh $SERVICE_DIR
+
+# add custom shortcut for XCode
+echo "add custom shortcut for XCode ..."
+defaults write com.apple.dt.Xcode NSUserKeyEquivalents -dict-add "my custom shortcut" $short_cut
+
+echo "restart xcode"
+killall Xcode
---
+++ myCustomShortcut.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+arr=()
+
+# eof 까지 파일 입력
+while read line || [[ -n "$line" ]]; do
+    arr+=("$line")
+done < /dev/stdin
+
+# 정렬
+arr_length=${#arr[@]}
+for (( i=0; i<${arr_length}-1; i++ ))
+do
+    for (( j=$i+1; j<${arr_length}; j++ ))
+    do
+        if [[ "${#arr[$i]}" < "${#arr[$j]}" ]]; then
+            temp=${arr[$i]}
+            arr[$i]=${arr[$j]}
+            arr[$j]=$temp
+        fi
+    done
+done
+
+# 출력
+for item in "${arr[@]}"
+do
+    echo "$item"
+done
